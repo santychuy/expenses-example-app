3ec09a1cbbbb8226ccf517a24fc2aa0fefee338c
Santiago Carrasco <jsanticc@hotmail.com>
2024-06-19 16:20:04 -0700

    feat: validatio schema for expenses

---
+++ server/validations/expenses.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(6, 'The title needs to be a minimum of 6 characters'),
+  amount: z.number().int().positive()
+});
+
+export const createExpenseSchema = expenseSchema.omit({ id: true });
+
+export type Expense = z.infer<typeof expenseSchema>;
